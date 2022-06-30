10b2098c6683ad07cabe5b7fc7fa6086c68d0d9f
AlierQ <1029369938@qq.com>
2022-07-01 00:51:36 +0800

    deploy

---
+++ precache-manifest.cd40eec0f518cbe83f156903c437ea86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98707da2cfab34eea289",
+    "url": "/Mint-vue-pages/css/app.6411aa8a.css"
+  },
+  {
+    "revision": "4abc402733144433a47426c3a67d2ede",
+    "url": "/Mint-vue-pages/index.html"
+  },
+  {
+    "revision": "98707da2cfab34eea289",
+    "url": "/Mint-vue-pages/js/app.32a046a1.js"
+  },
+  {
+    "revision": "21baa8d29d4644d43c3f",
+    "url": "/Mint-vue-pages/js/chunk-vendors.ae168445.js"
+  },
+  {
+    "revision": "3ce0f591737b5aebb6dc939fb4822c86",
+    "url": "/Mint-vue-pages/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Mint-vue-pages/robots.txt"
+  }
+]);
